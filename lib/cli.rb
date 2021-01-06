69633566c19dcca56e748139eb4d31d839f21934
Nathan Fedzen <nfedzen@hotmail.com>
2021-01-06 14:14:49 -0700

    Finish MVP

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,19 +3,26 @@ require 'rest-client'
 require 'json'
 #ef6b7de36c8aed58b9210b1226e7bc4d
 #api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+ActiveRecord::Base.logger = nil
 class Cli  
 
   def prompt
     TTY::Prompt.new
   end
 
+  def pause
+    puts "[Press Enter to Continue]"
+    gets
+  end
+
   def welcome
     system('clear')
+    
     puts "Welcome to the Ski Resort Weather app!"
     ask = prompt.yes?("Would you like to select a resort?")
     if ask
-      resort_name = list_resorts
-      weather resort_name
+      @resort_name = list_resorts
+      weather @resort_name
     else
       puts "Too bad"
       welcome()
@@ -27,10 +34,18 @@ class Cli
   end
 
   def weather resort_name
-    chosen_resort = Resort.all.find_by name: resort_name
+    chosen_resort = Resort.all.find_by name: @resort_name
     lat = chosen_resort[:lat]
     long = chosen_resort[:long]
-    weather = JSON.parse(RestClient.get("api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=ef6b7de36c8aed58b9210b1226e7bc4d"))
-    binding.pry
+    weather = JSON.parse(RestClient.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=imperial&appid=ef6b7de36c8aed58b9210b1226e7bc4d"))
+    current_weather weather
+    pause
+  end
+
+  def current_weather weather
+    puts "The current weather for #{@resort_name} is:"
+    puts "Temperature: #{weather["current"]["temp"]}"
+    puts "Conditions: #{weather["current"]["weather"][0]["main"]}"
+    puts "Wind Speed: #{weather["current"]["wind_speed"]} mph"
   end
 end
